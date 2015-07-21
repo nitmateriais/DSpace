nitmateriais/DSpace

commit 7b68f982e21d7777523b3574495d2cf423fb49ef

Reuse authority IDs returned by the authority plugin

dspace-api/src/main/java/org/dspace/authority/AuthorityValueGenerator.java
@@ -34,12 +34,10 @@ public class AuthorityValueGenerator {
 
 
     public static AuthorityValue generate(Context context, String authorityKey, String content, String field) {
-        AuthorityValue nextValue = null;
-
-        nextValue = generateRaw(authorityKey, content, field);
-
+        AuthorityValue nextValue = generateRaw(authorityKey, content, field);
 
         if (nextValue != null) {
+            boolean generateNewUUID = false;
             //Only generate a new UUID if there isn't one offered OR if the identifier needs to be generated
             if (StringUtils.isBlank(authorityKey)) {
                 // An existing metadata without authority is being indexed
@@ -47,18 +45,26 @@ public class AuthorityValueGenerator {
                 AuthorityValueFinder authorityValueFinder = new AuthorityValueFinder();
                 List<AuthorityValue> byValue = authorityValueFinder.findByExactValue(context, field, content);
                 if (byValue != null && !byValue.isEmpty()) {
-                    authorityKey = byValue.get(0).getId();
+                    nextValue.setId(byValue.get(0).getId());
                 } else {
-                    authorityKey = UUID.randomUUID().toString();
+                    generateNewUUID = true;
+
                 }
             } else if (StringUtils.startsWith(authorityKey, AuthorityValueGenerator.GENERATE)) {
-                authorityKey = UUID.randomUUID().toString();
+                if (StringUtils.isBlank(nextValue.getId())) {
+                    // Only generate a new UUID if an appropriate id was not already set
+                    // by the AuthorityValue implementation. This allows the implementation
+                    // to reuse existing Solr documents.
+                    generateNewUUID = true;
+                }
             }
 
-            nextValue.setId(authorityKey);
-            nextValue.updateLastModifiedDate();
-            nextValue.setCreationDate(new Date());
-            nextValue.setField(field);
+            if (generateNewUUID) {
+                nextValue.setId(UUID.randomUUID().toString());
+                nextValue.updateLastModifiedDate();
+                nextValue.setCreationDate(new Date());
+                nextValue.setField(field);
+            }
         }
 
         return nextValue;

commit 530a7307096a38bd45c46a484855829051b1790d

Fix index not generated regression caused by 7b68f982e21

dspace-api/src/main/java/org/dspace/authority/AuthorityValueGenerator.java
@@ -54,7 +54,7 @@ public class AuthorityValueGenerator {
                 if (StringUtils.isBlank(nextValue.getId())) {
                     // Only generate a new UUID if an appropriate id was not already set
                     // by the AuthorityValue implementation. This allows the implementation
-                    // to reuse existing Solr documents.
+                    // to preserve existing Solr document IDs.
                     generateNewUUID = true;
                 }
             }
@@ -63,8 +63,8 @@ public class AuthorityValueGenerator {
                 nextValue.setId(UUID.randomUUID().toString());
                 nextValue.updateLastModifiedDate();
                 nextValue.setCreationDate(new Date());
-                nextValue.setField(field);
             }
+            nextValue.setField(field);
         }
 
         return nextValue;